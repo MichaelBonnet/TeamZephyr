e5e599aac3f69bf60556da59da5abcf34053f255
jibarra436 <60481786+jibarra436@users.noreply.github.com>
2021-12-05 13:11:33 -0600

    Added comments and explanation

--- radio/rfm9x-base.py
+++ radio/rfm9x-base.py
@@ -1,4 +1,8 @@
-# Base Station Radio (Sends first)
+# Base Station Radio
+# Team Zephyr
+# Run script with 'python3 rfm9x-base.py' (may need sudo)
+
+# Imports
 import time
 import busio
 from digitalio import DigitalInOut, Direction, Pull
@@ -9,38 +13,65 @@ import adafruit_rfm9x
 CS = DigitalInOut(board.CE1)
 RESET = DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0) # Last argument is the frequency. LoRa supports 915 MHz and 868 MHz
 rfm9x.tx_power = 23
 
+# Variables initialized with default values
 command = None
 sending = True
 
+# Main loop for sending data to payload
+# Works as a remote shell via radio (LoRa module)
+# Can send any linux command to the remote system, and prints output.
+#
+# Preset Commands:
+# 'quit' : exits the loop and shuts down the script on BOTH sides (base station & payload)
 while True:
 
+    # If it's our turn to send (not currently listening for response)
 	if sending:
+    
+        # Get input command from user
 		command = input('[Base Station] Send command: ')
 		msg = bytes(command, 'utf-8')
+        
+        # Send the input to payload via radio
 		rfm9x.send_with_ack(msg)
 
+        # If the command was 'quit', break out of the loop (closes program)
 		if(command == 'quit'):
 			break
 
+        # Set the 'sending' flag to false so we listen on our next loop iteration
 		sending = False
+        
+    # If sending == false, we listen for {timeout} second(s) for any response from the payload
+    # Default timeout is 1 second, can be changed below on line 48
 	else:
 		# Timeout in case message is not acknowledged
 		timeout = 1   # [seconds]
 		timeout_start = time.time()
 
+        # While {timeout} second(s) have not passed, continue listening loop
 		while (time.time() < timeout_start + timeout):
+        
+            # Clear anything we may have in the 'packet' variable
 			packet = None
+            
+            # Attempt to recieve a message over radio (LoRa module) from payload station
 			packet = rfm9x.receive()
 
+            # Condition to check if our recieved packet contains any data
+            # If it is empty, continue loop without any output
+            # Else, decode the packet as a utf-8 encoded string, and print the output to console
 			if packet is None:
 				continue
 			else:
 				prev_packet = str(packet, 'utf-8')
 				print(prev_packet)
-			
+		
+        # Set the 'sending' flag back to true to return to sending mode
 		sending = True
-
+    
+    # Short wait to optimize performance
 	time.sleep(0.1)
--- radio/rfm9x-payload.py
+++ radio/rfm9x-payload.py
@@ -1,4 +1,8 @@
 # Payload Radio
+# Team Zephyr
+# Run script with 'python3 rfm9x-payload.py' (may need sudo)
+
+# Imports
 import time
 import busio
 from digitalio import DigitalInOut, Direction, Pull
@@ -13,35 +17,50 @@ import subprocess
 CS = DigitalInOut(board.CE1)
 RESET = DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0) # Last argument is the frequency. LoRa supports 915 MHz and 868 MHz
 rfm9x.tx_power = 23
 
+# Utiliy function to easily send replies back to base station
 def reply(str):
+    # Encodes message as utf-8 string
 	sending = bytes(str, 'utf-8')
+    # Sends the packet to base station
 	rfm9x.send(sending)
 
+# Main loop for listening for messages from base station
 while True:
-	packet = None
-	packet = rfm9x.receive()
-
+    # Clear anything we may have in the 'packet' variable
+    packet = None
+    # Attempt to recieve a message over radio (LoRa module) from payload station
+    packet = rfm9x.receive()
+    
+    # If we recieved nothing, continue loop
+    # Else, we attempt to parse the recieved message and run a command
 	if packet is None:
 		continue
 	else:
+        # Change the standard output to 'buffer' variable
 		sys.stdout = buffer = StringIO()
 
+        # Decode the received 'packet'
 		prev_packet = str(packet, 'utf-8')
 
-		# quit if we recv quit message, else run script
+		# Quit if we recieve 'quit' command, else run command
 		if(prev_packet == "quit"):
 			break
 		else:
+            # Run the command received, and print the output to the 'buffer' variable
 			print("[Payload] Running command : "+prev_packet)
 			print(subprocess.getoutput("sudo "+prev_packet))
 
-		# Send console output to base station and clear
+		# Send buffer value to base station
 		reply(buffer.getvalue())
-
+        
+        # Reset the standard output to the console
 		sys.stdout = sys.__stdout__
+        
+        # Print the value of the buffer in console
 		print(buffer.getvalue())
-
+    
+    # Wait to optimize performance
 	time.sleep(0.1)
