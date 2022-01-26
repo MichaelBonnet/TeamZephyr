0ed04d2744591fa76c80928d00b151cd6a6f49e1
Benjamin Knight <60989905+ben8622@users.noreply.github.com>
2022-01-26 16:00:25 -0600

    LED Class Completion
    Jorge

--- radio/LED.py
+++ radio/LED.py
@@ -1,13 +1,11 @@
 import time, sys
 import RPi.GPIO as GPIO
 
-#TODO: add the self keyword used in redOn and redOff to the rest of the color function as used in those functions
-
 class LED:
 	def __init__(self):
-		self.redPin = 11
-		self.greenPin= 13
-		self.bluePin= 15
+		self.red_pin = 11
+		self.green_pin = 13
+		self.blue_pin = 15
 	
 	def blink(self, pin):
 	    	GPIO.setwarnings(False)
@@ -15,68 +13,39 @@ class LED:
 	    	GPIO.setup(pin, GPIO.OUT)
 	    	GPIO.output(pin, GPIO.HIGH)
     
-	def turnOff(self, pin):
+	def off(self):
 	    	GPIO.setwarnings(False)
 	    	GPIO.setmode(GPIO.BOARD)
-	    	GPIO.setup(pin, GPIO.OUT)
-	    	GPIO.output(pin, GPIO.LOW)
+	    	GPIO.setup(self.red_pin, GPIO.OUT)
+	    	GPIO.output(self.red_pin, GPIO.LOW)
+	    	GPIO.setup(self.green_pin, GPIO.OUT)
+	    	GPIO.output(self.green_pin, GPIO.LOW)
+	    	GPIO.setup(self.blue_pin, GPIO.OUT)
+	    	GPIO.output(self.blue_pin, GPIO.LOW)
 	  	
-	def redOn(self):
-	    	self.blink(self.redPin)
+	def red(self):
+		self.off()
+		self.blink(self.red_pin)
 
-	def redOff(self):
-	    	self.turnOff(self.redPin)
+	def green(self):
+		self.off()
+		self.blink(self.green_pin)
 
-	def greenOn():
-	    	blink(greenPin)
+	def blue(self):
+		self.off()
+		self.blink(self.blue_pin)
 
-	def greenOff():
-	    	turnOff(greenPin)
+	def yellow(self):
+		self.off()
+		self.blink(self.red_pin)
+		self.blink(self.green_pin)
 
-	def blueOn():
-	    	blink(bluePin)
+	def cyan(self):
+		self.off()
+		self.blink(self.green_pin)
+		self.blink(self.blue_pin)
 
-	def blueOff():
-	    	turnOff(bluePin)
-
-	def yellowOn():
-	    	blink(redPin)
-	    	blink(greenPin)
-
-	def yellowOff():
-	    	turnOff(redPin)
-	    	turnOff(greenPin)
-
-	def cyanOn():
-		blink(greenPin)
-		blink(bluePin)
-
-	def cyanOff():
-	    	turnOff(greenPin)
-	    	turnOff(bluePin)
-
-	def magentaOn():
-	    	blink(redPin)
-	    	blink(bluePin)
-
-	def magentaOff():
-	    	turnOff(redPin)
-	    	turnOff(bluePin)
-
-	def whiteOn():
-	    	blink(redPin)
-	    	blink(greenPin)
-	    	blink(bluePin)
-
-	def whiteOff():
-	    	turnOff(redPin)
-	    	turnOff(greenPin)
-	    	turnOff(bluePin)
-	
-
-#Example code that calls the function
-drone = LED()
-drone.redOff()
-	
-
-	  	    	
+	def magenta(self):
+		self.off()
+		self.blink(self.red_pin)
+		self.blink(self.blue_pin)
