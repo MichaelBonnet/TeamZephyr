41345b31d3168d923de700d52bfe3478d5f304fa
Ben, BASE STATION <benjaminjknight0987@gmail.com>
2022-01-26 15:12:59 -0600

    base station script first iteration

---
+++ radio_new/base.py
@@ -0,0 +1,26 @@
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+import adafruit_rfm9x
+import RPi.GPIO as GPIO
+
+def main():
+    # init LoRa
+    CS = DigitalInOut(board.CE1)
+    RESET = DigitalInOut(board.D25)
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 433.0)
+    rfm9x.tx_power = 23
+    print("rfm9x initialized")
+
+    recv = ""
+
+    while recv != "quit":
+        recv = rfm9x.receive(with_ack=True, timeout=1000)
+        recv = str(packet, "utf-8") if recv != None else ""
+        
+        #if recv != "":
+        print(recv)
+
+if __name__ == "__main__":
+    main()
